f1aa5a03ae2a18fd6c8f1eb8abcecd3cdcb054d3
Midiquin <Midiquin@users.noreply.github.com>
2021-03-15 03:44:51 -0400

    update

---
+++ Mod9.R
@@ -0,0 +1,46 @@
+library(ggplot2)
+data <- read.csv("Data\\primates.csv")
+
+ggplot(data, aes(x=Bodywt, y=X, size=Brainwt, color=X))+
+  geom_point()+
+  ylab("Brain weight in g")+
+  xlab("Body weight in kg")+
+  labs(
+    color="Primate",
+    size="Brain weight in g"
+  )
+
+ggplot(data, aes(x=X, y=Bodywt)) +
+  geom_segment( aes(x=X, xend=X, y=1, yend=Bodywt), color="grey") +
+  geom_point( color="orange", size=4) +
+  theme_light() +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.x = element_blank()
+  ) +
+  xlab("") +
+  ylab("Body weight in g")
+
+
+ggplot(data, aes(x=Bodywt, y=X, fill=Brainwt))+
+  geom_bar(stat = "identity")+
+  ylab("Primate")+
+  xlab("Body Weight in g")+
+  labs(
+    fill="Brain weight in kg"
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
